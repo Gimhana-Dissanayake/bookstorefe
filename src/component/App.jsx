33620ee139783512c1cb838dc227650aa3a64dd5
GimhanaDissanayake <gimhanadev@gmail.com>
2022-07-14 00:53:54 +0530

    42. BookListItem compoenent

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,15 +1,15 @@
 
 
 
-import axios from "axios";
-import baseUrl from "../config";
+// import axios from "axios";
+// import baseUrl from "../config";
 import Layout from './layout/Layout'
 import BookContainer from "./book/BookContainer";
 
 function App() {
-  axios(`${baseUrl}/api/v1/books`).then((books) => {
-    console.log(books);
-  });
+  // axios(`${baseUrl}/api/v1/books`).then((books) => {
+  //   console.log(books);
+  // });
 
   return (
     <Layout>
--- src/component/book/BookContainer.jsx
+++ src/component/book/BookContainer.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import {Box} from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
+import getBooksAction from '../../module/book/bookAction';
+import { getBooksSelector } from '../../module/book/bookSelector'
 import BookFilter from './BookFilter';
 import styles from './BookStyles';
+import BookList from './BookList'
 
 const BookContainer = () => {
+
+    const dispatch = useDispatch()
+  
+    useEffect(()=>{
+      dispatch(getBooksAction())
+    },[dispatch])
+
+    const books = useSelector(getBooksSelector)
+
+    console.log(books)
 
     const classes = styles()
 
@@ -11,7 +25,7 @@ const BookContainer = () => {
     <Box className={classes.bookContainer}>
         <BookFilter/>
         <Box className={classes.bookList}>
-            Here we will display all books.
+         <BookList books={books}/>
         </Box>
     </Box>
   )
